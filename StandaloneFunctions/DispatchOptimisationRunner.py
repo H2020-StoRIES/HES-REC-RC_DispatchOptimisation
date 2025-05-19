73dc805290289f57a4a546182bd6825654f4b112
szata <szata@dtu.dk>
2025-05-19 12:26:05 +0200

    Info section

--- StandaloneFunctions/DispatchOptimisationRunner.py
+++ StandaloneFunctions/DispatchOptimisationRunner.py
@@ -8,8 +8,7 @@ Usage:
     path to the data file in the config.
 
 Output:
-    Shows result summary in the console and, if selected in the user settings,
-    plots and/or stores detailed results.
+    Shows results the in log repository.
 
 Authors:
     Nils Müller
@@ -22,7 +21,6 @@ Authors:
 import os
 import sys
 import yaml
-import logging
 import pandas as pd
 from time import time
 import sys
@@ -41,33 +39,16 @@ def run_daily_schedule(
 ):
     if plot_variables is None:
         plot_variables = ['pBt_A', 'et_A', 'pBt_C', 'et_C', 'qBt_A', 'Qt_A', 'pGrid', 'qGrid', 'q2p', 'pBt_RC']
-
-    logging.basicConfig(level=logging.INFO, format='%(message)s', handlers=[logging.StreamHandler(sys.stdout)])
-
-    logging.info(f"Reading config file {config_filename}...")
     with open(os.path.join(config_filename), 'r') as file:
         user_config = yaml.safe_load(file)
 
     config = initialize_config(default_config, user_config)
-
-    logging.info(f"Running daily schedule optimization ...")
     Daily_results = Run_Daily_Schedule_Optimization(config=config)
 
-    logging.info(f"Result summary:")
-    logging.info(f"   Optimization status: {Daily_results[1]['Solver_status']}")
-    logging.info(f"   Total cost: {round(Daily_results[1]['Cost_upper_bound'], 2)} €")
-    logging.info(f"   Yearly cost: {round(Daily_results[1]['Cost_upper_bound'] * 365, 2)} €")
-
     result_df = pd.DataFrame({key: Daily_results[1][key] for key in plot_variables})
-    # print(result_df)
-
-    # result_df.to_csv(f"./Results/DailyScheduleOptimization/{result_df}")
     total_cost = round(Daily_results[1]['Cost_upper_bound'], 2)
     yearly_cost = round(Daily_results[1]['Cost_upper_bound'] * 365, 2)
     config_updated= Result_Update(config, Daily_results[1])
-    # config_updated['Total_cost']= round(Daily_results[1]['Cost_upper_bound'],2)
-    # config_updated['Yearly_cost']= round(Daily_results[1]['Cost_upper_bound']*365,2)
-    # config_updated['Solver_status']= Daily_results[1]['Solver_status']
     with open(os.path.join(f'{config_filename}'), 'w') as file:
         yaml.dump(config_updated,file)
 
